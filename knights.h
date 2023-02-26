bea2f285df3495060d5616e0a3a3ca682b8cd69a
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2023-02-26 18:56:48 -0500

    added forgotten header

--- knights.h
+++ knights.h
@@ -2,5 +2,6 @@
 
 void fill(int board[5][5]);
 void print(int board[5][5]);
+bool done(int board[5][5]);
 bool isValid(int board[5][5],int posx, int posy);
 void solve(int board[5][5],int posx, int posy, int mvs);
