123229b68bfa57853554bff29c62a1b3dffef654
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2023-02-26 18:54:38 -0500

    bug fixed

--- knights.cpp
+++ knights.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <iomanip>
 
+
 int rowpos[8] = {2, 1, -1, -2, -2, -1, 1, 2};
-int colpos[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+int colpos[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
+
 
 void fill(int board[5][5]){
     for(int i = 0;i < 5; i++){
@@ -20,6 +22,17 @@ void print(int board[5][5]){
     }
 }
 
+bool done(int board[5][5]){
+    for(int i = 0;i < 5;i++){
+        for(int j = 0;j<5;j++){
+            if(board[i][j] == -1){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 bool isValid(int board[5][5], int posx, int posy){
     return (board[posx][posy] == -1 && posx >= 0 && posx < 5 && posy >= 0 && posy < 5);
 }
@@ -29,10 +42,14 @@ void solve(int board[5][5], int posx, int posy, int mvs){
         return;
     }
     board[posx][posy] = mvs;
+
     for(int i = 0;i < 8;i++){
         solve(board, posx + rowpos[i], posy + colpos[i], mvs + 1);
     }
-    if (mvs == 25){
+
+    if (done(board)){
         return;
     }
+    
+    board[posx][posy] = -1;
 }
